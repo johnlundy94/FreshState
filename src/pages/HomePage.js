f3ea681c89a18ac295afa96cc834b09e1ba4f45f
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-03-05 09:00:22 -0700

    Rendered title of homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Fresh State Kombucha Cocktail</h1>
+      <br />
+      <h2>High Quality Ingredients And Exciting Flavors!</h2>
+    </div>
+  );
+}
