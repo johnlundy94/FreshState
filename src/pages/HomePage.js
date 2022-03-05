c0ff7bfb5feb1f1e499db98f39caee56fab2d848
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-03-05 09:08:24 -0700

    Added rest of base homepage.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,50 @@
 export default function HomePage() {
   return (
-    <div>
-      <h1>Fresh State Kombucha Cocktail</h1>
+    <>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#about">About Us</a>
+            </li>
+            <li>
+              <a href="#product">Products</a>
+            </li>
+            <li>
+              <a href="#contact">Contact Us</a>
+            </li>
+            <li>
+              <a href="#location">Locations</a>
+            </li>
+            <li>
+              <a href="/login">Order</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <h1>Fresh State Kombucha Cocktail</h1>
+        <br />
+        <h2>High Quality Ingredients And Exciting Flavors!</h2>
+      </div>
       <br />
-      <h2>High Quality Ingredients And Exciting Flavors!</h2>
-    </div>
+      <h3>About Us</h3>
+      <div>
+        <br />
+        <p>
+          After years of being in the kombucha industry, we have seen our
+          friends, community, and ourselves making our weekend and evening
+          cocktails with kombucha. Why not just give everyone the ability to
+          enjoy these handcrafted kombucha cocktails at their own convenience.
+          No time to mix up a cocktail? We got you. Want to take your favorite
+          kombucha cocktail outdoors without hauling all the ingredients and
+          tools to make it? We got you. Looking for a ready to drink cocktail
+          that's low in sugar with no artificial ingredients? We got you! With
+          flavors like TIki Style Guava Rum and a Grapefruit Vodka, you can pour
+          these refreshing cocktails over ice with split between friends or
+          drink straight from the can!
+        </p>
+      </div>
+    </>
   );
 }
