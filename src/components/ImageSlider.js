65c569492dc23949cc02e0216d2488020f960e83
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-03-09 08:06:35 -0700

    Started product slider.

---
+++ src/components/ImageSlider.js
@@ -0,0 +1,61 @@
+<>
+<div class="products">
+  <h3 id="product">Products</h3>
+</div>
+<div class="slideshow-container">
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 6</div>
+    <img src="/images/download.png" style="width: 25%" />
+    <div class="text">Tiki Style Guava Rum</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 6</div>
+    <img src="/images/download.png" style="width: 25%" />
+    <div class="text">Grapefruit Vodka</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 6</div>
+    <img src="/images/download.png" style="width: 25%" />
+    <div class="text">Watermelon Mint</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">4 / 6</div>
+    <img src="/images/download.png" style="width: 25%" />
+    <div class="text">Sunset Rose</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">5 / 6</div>
+    <img src="/images/download.png" style="width: 25%" />
+    <div class="text">Mango</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">6 / 6</div>
+    <img src="/images/download.png" style="width: 25%" />
+    <div class="text">Island Pineapple</div>
+  </div>
+
+  <a class="prev" onclick="plusSlides(-1)"><</a>
+  <a class="next" onclick="plusSlides(1)">></a>
+</div>
+<br />
+
+
+<div class="product-info">
+  Kombucha with an extra kick is no stranger to people who are looking for
+  a new way to enjoy adult beverages. Using traditional kombucha brewing
+  methods and fresh all natural ingredients, Fresh State Hard Kombucha is
+  an approachable hard kombucha that will give you the benefits of
+  kombucha with a little extra kick!" WIth Flavors like Watermelon Mint,
+  Sunset Rose, Mango, and Island Pineapple you can enjoy these at any
+  occasion.
+
+  <br />
+  <br />
+
+  </div>
+  </>
